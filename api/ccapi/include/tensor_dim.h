53f5681eca8dc497a97f601f5657848f3bc9a8e4
jijoong.moon <jijoong.moon@samsung.com>
2023-12-19 13:57:44 +0900

    [ Tensor ] Add BCQHW with BiQGEMM
    This PR includes
         supporting BCQHW weight type
         include BiQGEMM Lib
    
    **Changes proposed in this PR:**
    - Added TOC generator for README.md
    
    Resolves:
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- api/ccapi/include/tensor_dim.h
+++ api/ccapi/include/tensor_dim.h
@@ -48,14 +48,15 @@ public:
   enum class Format { NCHW, NHWC };
 
   /**
-   * @brief Tensor Data Type. Currently QINT4, QINT8, FP16 & FP32 Support
+   * @brief Tensor Data Type. Currently QINT4, QINT8, FP16, FP32 & BCQ32 Support
    *
    */
   enum class DataType {
     QINT4, /** quantized int 4*/
     QINT8, /** quantized int 8*/
     FP16,  /** half precision */
-    FP32   /** single precision */
+    FP32,  /** single precision */
+    BCQ32  /** packed BCQ*/
   };
 
   /**
@@ -95,7 +96,7 @@ public:
    * @brief     Creator of TensorDim with Format & DataType
    *
    * @param fm format NCHW | HNWC
-   * @param fm DataType QINT4 | QINT8 | FP16 | FP32
+   * @param fm DataType QINT4 | QINT8 | FP16 | FP32 | BCQ32
    * @param eff_dim_flag_ effective dimension flag (1 means it's effective)
    * @param dyn_dim_flag_ dynamic dimension flag (1 means it's unspecified)
    */
@@ -198,7 +199,7 @@ public:
    * @param h height
    * @param w width
    * @param fm format NCHW | HNWC
-   * @param d_type Data Type QINT4 | QINT8 | FP16 | FP32
+   * @param d_type Data Type QINT4 | QINT8 | FP16 | FP32 | BCQ32
    * @param eff_dim_flag_ dimension bit flag to calculate the dynamic
    * dimension, rightmost is width
    */
@@ -227,7 +228,7 @@ public:
    *
    * @param shape shape of format
    * @param fm format NCHW | HNWC
-   * @param d_type data type QINT4 | QINT8 | FP16 | FP32
+   * @param d_type data type QINT4 | QINT8 | FP16 | FP32 | BCQ32
    */
   TensorDim(const std::string &shape, TensorDim::Format fm,
             TensorDim::DataType d_type = TensorDim::DataType::FP32);
