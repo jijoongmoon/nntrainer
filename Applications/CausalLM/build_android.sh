a5213a7c5c88ffee674c8be8622200d3221138f3
Eunju Yang <ej.yang@samsung.com>
2026-03-18 11:59:20 +0900

    [CausalLM] fix mmap read in tie-word-embedding
    - This commit fixes the bugs in tie-word-embedding
    - Tie-word-embedding didn't support read with mmap before.
    - This commit adds read with mmap and remove enforced flag of mmap
      disablement when enable-transformer is enabled
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- Applications/CausalLM/build_android.sh
+++ Applications/CausalLM/build_android.sh
@@ -25,7 +25,7 @@ if [ ! -f "$NNTRAINER_ROOT/builddir/android_build_result/lib/arm64-v8a/libnntrai
     if [ -d "$NNTRAINER_ROOT/builddir" ]; then
         rm -rf builddir
     fi
-    ./tools/package_android.sh -Dmmap-read=false
+    ./tools/package_android.sh
 else
     echo "nntrainer for Android already built."
 fi
